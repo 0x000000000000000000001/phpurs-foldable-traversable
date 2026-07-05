c0675c84943b3483b5b989ae5f56bcd9f2251abd
0x1 <kevin.francart3@gmail.com>
2026-07-05 20:08:23 +0200

    perf(ffi): replace phpurs_uncurry overhead with inline uncurrying closures

--- src/Data/Foldable.php
+++ src/Data/Foldable.php
@@ -1,52 +1,22 @@
 <?php
-if (!function_exists('phpurs_uncurry2')) {
-function phpurs_uncurry2($fn) {
-    return function($a, $b = null) use ($fn) {
-        if (func_num_args() < 2) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry2($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b);
-    };
-}
-function phpurs_uncurry3($fn) {
-    return function($a, $b = null, $c = null) use ($fn) {
-        if (func_num_args() < 3) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry3($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c);
-    };
-}
-function phpurs_uncurry4($fn) {
-    return function($a, $b = null, $c = null, $d = null) use ($fn) {
-        if (func_num_args() < 4) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry4($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c)($d);
-    };
-}
-function phpurs_uncurry5($fn) {
-    return function($a, $b = null, $c = null, $d = null, $e = null) use ($fn) {
-        if (func_num_args() < 5) {
-            $__args = func_get_args();
-            return function(...$more) use ($fn, $__args) {
-                return phpurs_uncurry5($fn)(...array_merge($__args, $more));
-            };
-        }
-        return $fn($a)($b)($c)($d)($e);
-    };
-}
-}
 
-
-
-$Data_Foldable_foldrArray = phpurs_uncurry3(function($f) { return function($init) use(&$f) { return function($xs) use(&$f, &$init) { $acc = $init; for($i=count($xs)-1; $i>=0; $i--) { $acc = $f($xs[$i])($acc); } return $acc; }; }; });
-$Data_Foldable_foldlArray = phpurs_uncurry3(function($f) { return function($init) use(&$f) { return function($xs) use(&$f, &$init) { $acc = $init; foreach($xs as $x) { $acc = $f($acc)($x); } return $acc; }; }; });
+$Data_Foldable_foldrArray = function($f, $init = null, $xs = null) {
+    if (func_num_args() < 3) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args) {
+            global $Data_Foldable_foldrArray;
+            return $Data_Foldable_foldrArray(...array_merge($__args, $more));
+        };
+    }
+    $acc = $init; for($i=count($xs)-1; $i>=0; $i--) { $acc = $f($xs[$i])($acc); } return $acc;
+};
+$Data_Foldable_foldlArray = function($f, $init = null, $xs = null) {
+    if (func_num_args() < 3) {
+        $__args = func_get_args();
+        return function(...$more) use ($__args) {
+            global $Data_Foldable_foldlArray;
+            return $Data_Foldable_foldlArray(...array_merge($__args, $more));
+        };
+    }
+    $acc = $init; foreach($xs as $x) { $acc = $f($acc)($x); } return $acc;
+};

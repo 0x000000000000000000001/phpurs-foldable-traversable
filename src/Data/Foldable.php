aa868c1e9227455a4cfb0bf42fb8ca3597ddbc15
0x1 <kevin.francart3@gmail.com>
2026-07-06 18:59:02 +0200

    feat: migrate FFI to exports array format

--- src/Data/Foldable.php
+++ src/Data/Foldable.php
@@ -1,22 +1,26 @@
 <?php
 
-$Data_Foldable_foldrArray = function($f, $init = null, $xs = null) {
+$foldrArray = function($f, $init = null, $xs = null) use (&$foldrArray) {
     if (func_num_args() < 3) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Data_Foldable_foldrArray;
-            return $Data_Foldable_foldrArray(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$foldrArray) {
+
+            return $foldrArray(...array_merge($__args, $more));
         };
     }
     $acc = $init; for($i=count($xs)-1; $i>=0; $i--) { $acc = $f($xs[$i])($acc); } return $acc;
 };
-$Data_Foldable_foldlArray = function($f, $init = null, $xs = null) {
+$foldlArray = function($f, $init = null, $xs = null) use (&$foldlArray) {
     if (func_num_args() < 3) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Data_Foldable_foldlArray;
-            return $Data_Foldable_foldlArray(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$foldlArray) {
+
+            return $foldlArray(...array_merge($__args, $more));
         };
     }
     $acc = $init; foreach($xs as $x) { $acc = $f($acc)($x); } return $acc;
 };
+
+$exports['foldrArray'] = $foldrArray;
+$exports['foldlArray'] = $foldlArray;
+return $exports;
